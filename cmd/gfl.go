87f23b68dcfa0dc1c6c9d830e9a8dba7b3f0c9e2
fumiyauehara <uehara@mediaxis.jp>
2021-04-12 21:12:18 +0900

    update add process to print only file of type.

--- cmd/gfl.go
+++ cmd/gfl.go
@@ -1,6 +1,10 @@
 package cmd
 
 import (
+	"fmt"
+	"os"
+	"path/filepath"
+
 	"github.com/spf13/cobra"
 )
 
@@ -8,6 +12,29 @@ var (
 	gflCmd = &cobra.Command{
 		Use: "gfl",
 		RunE: func(cmd *cobra.Command, args []string) error {
+			//			files, err := ioutil.ReadDir("./test")
+			//			if err != nil {
+			//				panic(err)
+			//			}
+			//
+			//			for _, f := range files {
+			//				fmt.Println(f.Name())
+			//			}
+
+			err := filepath.Walk("./test", func(path string, info os.FileInfo, err error) error {
+				if err != nil {
+					panic(err)
+				}
+				if !info.IsDir() {
+					fmt.Printf("path: %#v\n", path)
+				}
+				return nil
+
+			})
+			if err != nil {
+				panic(err)
+			}
+
 			return nil
 		},
 	}
