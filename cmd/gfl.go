a7a4c5118c52a3b3cbe733e2d4e0fcd9408b56da
fumiyauehara <uehara@mediaxis.jp>
2021-04-13 16:33:35 +0900

    update Change output destination file to specified option.

--- cmd/gfl.go
+++ cmd/gfl.go
@@ -9,7 +9,8 @@ import (
 )
 
 var (
-	src string
+	src  string
+	dest string
 
 	gflCmd = &cobra.Command{
 		Use: "gfl",
@@ -20,7 +21,13 @@ var (
 				panic(err)
 			}
 
-			savingFile, err := os.Create("./test_output.txt")
+			d, err := cmd.Flags().GetString("destination")
+			if err != nil {
+				panic(err)
+			}
+
+			// 相対パスor絶対パス指定(./や/がない場合はエラーになる)
+			savingFile, err := os.Create(d)
 			if err != nil {
 				panic(err)
 			}
@@ -58,4 +65,8 @@ func init() {
 	rootCmd.AddCommand(gflCmd)
 	gflCmd.Flags().StringVarP(&src, "source", "s", "", "source directory")
 	_ = gflCmd.MarkFlagRequired("source")
+
+	gflCmd.Flags().StringVarP(&dest, "destination", "d", "", "destination directory")
+	// TODO 一旦マストにしているがない場合は標準出力にしたい。
+	_ = gflCmd.MarkFlagRequired("dest")
 }
