eba4a811f8baaf13a629defedf7f0d5b6be72aa7
fumiyauehara <uehara@mediaxis.jp>
2021-04-13 17:09:56 +0900

    update To stdout from file output.

--- cmd/gfl.go
+++ cmd/gfl.go
@@ -9,53 +9,28 @@ import (
 )
 
 var (
-	src  string
-	dest string
+	src string
 
 	gflCmd = &cobra.Command{
 		Use: "gfl",
 		RunE: func(cmd *cobra.Command, args []string) error {
-
 			s, err := cmd.Flags().GetString("source")
 			if err != nil {
 				panic(err)
 			}
-
-			d, err := cmd.Flags().GetString("destination")
-			if err != nil {
-				panic(err)
-			}
-
-			// 相対パスor絶対パス指定(./や/がない場合はエラーになる)
-			savingFile, err := os.Create(d)
-			if err != nil {
-				panic(err)
-			}
-			defer savingFile.Close()
-
 			err = filepath.Walk(s, func(path string, info os.FileInfo, err error) error {
-
 				if err != nil {
 					panic(err)
 				}
-
 				if !info.IsDir() {
-
-					if _, err := savingFile.WriteString(path); err != nil {
-						panic(err)
-					}
-					fmt.Fprintf(savingFile, "\n")
-
+					fmt.Println(path)
 				}
-
 				return nil
 
 			})
-
 			if err != nil {
 				panic(err)
 			}
-
 			return nil
 		},
 	}
@@ -66,7 +41,7 @@ func init() {
 	gflCmd.Flags().StringVarP(&src, "source", "s", "", "source directory")
 	_ = gflCmd.MarkFlagRequired("source")
 
-	gflCmd.Flags().StringVarP(&dest, "destination", "d", "", "destination directory")
-	// TODO 一旦マストにしているがない場合は標準出力にしたい。
-	_ = gflCmd.MarkFlagRequired("dest")
+	//gflCmd.Flags().StringVarP(&dest, "destination", "d", "", "destination directory")
+	//// TODO 一旦マストにしているがない場合は標準出力にしたい。
+	//_ = gflCmd.MarkFlagRequired("dest")
 }
